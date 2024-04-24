d77595f595bd4ca37d1b65c8c100c882df9b015d
Paola <Paola@DESKTOP-L53JRFL>
2024-04-24 14:44:42 -0300

    Adicionar arquivos de projeto.

---
+++ areaCirculo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double raio, raioQuadrado, pi, area;
+
+        pi = 3.14159;
+        raio= double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        raioQuadrado = (Math.Pow(raio, 2));
+
+        area = pi * raioQuadrado;
+
+        Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+
+        Console.Read();
+        
+
+    }
+
+}
